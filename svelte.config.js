569c6eda6e148872f091133185ef590b91e0dc95
Ayush Jain <lolmafia66@gmail.com>
2026-06-30 01:38:12 +0530

    Switch to @sveltejs/adapter-vercel for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		})
+		adapter: adapter()
 	}
 };
 
