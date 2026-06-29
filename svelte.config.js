0d07375b7d39aa16f22d70974f545469210d9d95
Ayush Jain <lolmafia66@gmail.com>
2026-06-30 01:46:29 +0530

    Switch to adapter-static + netlify.toml for Netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
+		})
 	}
 };
 
