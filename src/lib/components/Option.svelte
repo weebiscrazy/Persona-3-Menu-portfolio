d18e7d9bb00b4b952f50b5250873c54c8a099703
Ayush Jain <lolmafia66@gmail.com>
2026-07-11 13:22:10 +0530

    Tune selector envelope: x-scale 0.7+2.5, y-scale 5.0, translateY -15

--- src/lib/components/Option.svelte
+++ src/lib/components/Option.svelte
@@ -27,7 +27,7 @@
   const selectorPath = "M 24.853754, 93.31573 135.14625, 49.684266 114.14751, 97.331142 Z";
   const selectorBackgroundPath = "M 12.7428765,95.50088 144.25712,47.499123 116.75625,95.465764 Z";
   const selectorMaskId = $derived(`selector-mask-${index}`);
-  const selectorTransform = $derived(`translate(-60, -10) rotate(8, 0, 100) scale(${option.name.replaceAll(" ", "").length * 0.8 + 3.0}, 6.0)`);
+  const selectorTransform = $derived(`translate(-60, -15) rotate(8, 0, 100) scale(${option.name.replaceAll(" ", "").length * 0.7 + 2.5}, 5.0)`);
 
   $effect(() => {
     if (isMobile) return;
